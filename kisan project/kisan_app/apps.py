2ee070d4809722198bee9c5f5c90d6c7b7da3037
MANISH <157676779+Manishyadav066@users.noreply.github.com>
2025-10-08 14:10:49 +0530

    Delete kisan project directory

--- kisan project/kisan_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class KisanAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kisan_app'
--- kisan project/start_server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Kisan Project Server...
-echo ========================================
-echo Make sure you have activated your virtual environment
-echo If not, run: venv\Scripts\activate
-echo ========================================
-timeout /t 2 /nobreak >nul
-python manage.py runserver
-pause
